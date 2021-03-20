7e1440c80240f0a4ce3e1e78cbf491763dea4449
Stanislav Bezkorovainyi <stanislavbezkor@gmail.com>
2021-03-20 14:29:33 +0200

    Only support social icons

--- src/blocks/SocialBlock.vue
+++ src/blocks/SocialBlock.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="socialIcons">
-    <a href="https://medium.com/matter-labs" target="_blank" class="socialItem">
-      <i class="fab fa-medium-m"/>
-    </a>
-    <a href="https://github.com/matter-labs/zksync" target="_blank" class="socialItem">
-      <i class="fab fa-github"/>
-    </a>
-    <a href="https://twitter.com/the_matter_labs" target="_blank" class="socialItem">
-      <i class="fab fa-twitter"/>
-    </a>
-    <a href="https://gitter.im/matter-labs/community" target="_blank" class="socialItem">
+    <a href="https://gitter.im/matter-labs/zksync" target="_blank" class="socialItem">
       <i class="fab fa-gitter"/>
     </a>
-    <a href="https://t.me/zksync" target="_blank" class="socialItem">
+    <a href="https://discord.com/invite/px2aR7w" target="_blank" class="socialItem">
+      <i class="fab fa-discord"/>
+    </a>
+    <a href="https://t.me/zksync_support" target="_blank" class="socialItem">
       <i class="fab fa-telegram-plane"/>
     </a>
   </div>
